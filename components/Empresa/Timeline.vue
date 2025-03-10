f4d5fc43a8154ef670c06b2c906dded20953d574
santiagogirolami <santiagogirolami@gmail.com>
2025-03-10 09:07:11 -0300

    img empresas

--- components/Empresa/Timeline.vue
+++ components/Empresa/Timeline.vue
@@ -5,7 +5,7 @@
   >
     <div
       data-aos="slide-up"
-      class="flex w-1/2 flex-col text-primary gap-6 z-20"
+      class="flex w-1/2 flex-col text-primary z-20"
       :class="par ? 'self-start' : 'self-end text-end'"
     >
       <div
@@ -21,17 +21,33 @@
           class="absolute rounded-full z-10 h-8 w-8 bg-sky-500 -bottom-4 -left-4 animate-ping"
         ></span>
       </div>
+
       <div
         class="text-lg lg:text-2xl lg:px-11 px-5 z-0"
         :data-aos="par ? 'slide-left' : 'slide-right'"
       >
+        <div
+          v-if="datos.banner"
+          :style="{ backgroundImage: `url(/img/Empresa/${datos.banner})` }"
+          class="w-full h-20 bg-cover bg-center bg-tertiary"
+          alt="Banner"
+        ></div>
         {{ datos.info }}
       </div>
     </div>
     <div
       class="absolute h-full border-4 lg:border-8 bg-primary border-primary flex shadow-2xl"
       :class="id === 0 ? 'rounded-t-full' : ''"
-      style="left: 50%; transform: translateX(-50%)"
+      style="
+        left: 50%;
+        transform: translateX(-50%);
+        background: linear-gradient(
+          to right,
+          transparent,
+          var(--color-tertiary),
+          transparent
+        );
+      "
     ></div>
   </div>
 </template>
--- pages/Empresa.vue
+++ pages/Empresa.vue
@@ -25,36 +25,52 @@ const data = [
   {
     year: "1960",
     info: "Jacinto Amatriain realiza la primer importación de rodamientos desde Inglaterra.",
+    banner: "Empresa1.jpg",
   },
   {
     year: "1974",
     info: "Se adquiere la distribución oficial de la marca NTN-SNR y AYS FERSA",
+    banner: "Empresa2.jpg",
   },
   {
     year: "1983",
     info: 'Se compra una vieja casona en San Martín 2437 de Rosario. La empresa comienza a girar bajo el nombre de "Jacinto Amatriain e Hijos ", a cargo de Andrés Amatriain.',
+    banner: "Empresa3.jpg",
   },
   {
     year: "2004",
     info: "La empresa comienza a girar bajo el nombre 'Roda Service' de Andrés Amatriain. También se adquiere la distribución oficial de las marcas NSK, ARB y HCH",
+    banner: "Empresa4.jpg",
   },
   {
     year: "2009",
     info: "Se adquiere la distribución oficial de las marcas IKO, MBS Y ROLLWAY",
+    banner: "Empresa5.jpg",
   },
   {
     year: "2014",
     info: "Se adquiere la distribucion oficial de las marcas CMB, RLM y KG",
+    banner: "Empresa6.jpg",
   },
   {
     year: "2019",
     info: "Se adquiere la distribucion oficial de la marca KOYO",
+    banner: "Empresa7.jpg",
   },
   {
     year: "2020",
     info: "Se realiza la demolición de la casona y comienza la construcción del nuevo local",
+    banner: "Empresa8.jpg",
   },
-  { year: "2024", info: "Se constituye RODA SERVICE SRL" },
-  { year: "2025", info: "Se inaugura el nuevo local comercial" },
+  {
+    year: "2024",
+    info: "Se constituye RODA SERVICE SRL",
+    banner: "Empresa9.jpg",
+  },
+  {
+    year: "2025",
+    info: "Se inaugura el nuevo local comercial",
+    banner: "Empresa10.jpg",
+  },
 ];
 </script>
